17ad7c567ea9922a38d979ea75b170f9528f75ad
Keith Rodriguez <rodriguez.keith@gmail.com>
2017-10-19 19:11:32 -0500

    Add Expensify styling

--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { LoginPage } from '../../components/LoginPage'
+import { LoginPage } from '../../components/LoginPage';
 
 test('should render login page correctly', () => {
   const wrapper = shallow(<LoginPage />);
